229a92d84173fea9a8215e9b85a1727135a1226c
gandradep <eventsonido@gmail.com>
2023-08-10 11:09:33 +0800

    adding function to button to change state

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super(); //underline constructor from classes extending from.
+    this.state = {
+      name: 'Gabri  el'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => {
+             this.setState({ name:'Changed'})
+          }}> Change name</button>
         </header>
       </div>
     );
-
   }
-
 }
 
 export default App;
